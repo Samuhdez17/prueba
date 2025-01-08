8545dbd2fdd12c3a5e55bf420e72df115f853d03
Samuhdez17 <lf254333@gmail.com>
2025-01-08 13:05:22 +0100

    testeo para añadir un .java y modificarlo

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int Cantidad;
+        int Contador = 0;
+        int Numeros_sumar;
+        int Resultado = 0;
+
+        System.out.println("Cuántos números quieres sumar");
+        Cantidad = teclado.nextInt();
+
+        do {
+            System.out.println("Que numero quieres sumar");
+            Numeros_sumar = teclado.nextInt();
+            Contador++;
+            Resultado = Resultado + Numeros_sumar;
+        } while (Contador < Cantidad);{
+            System.out.println("Tu resultado es " + Resultado);
+        }
+
+jeje soy hacker esto esta hackiao
+    }
+}
